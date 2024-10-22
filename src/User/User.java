ba648ecf82d5bf7887e659f455ba58249151aaec
Knkydd <alexpesman@gmail.com>
2024-10-22 08:08:00 +0500

    second commit

---
+++ src/User/User.java
@@ -0,0 +1,11 @@
+package User;
+
+public class User {
+    private String kingdomName;
+    private int root;
+
+    public User(String kingdomName, int root){
+        this.kingdomName = kingdomName;
+        this.root = root;
+    }
+}
