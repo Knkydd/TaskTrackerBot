fe7134606a0c82c673b971c4800436a5fa660087
Knkydd <143598558+Knkydd@users.noreply.github.com>
2024-12-27 00:15:47 +0500

    Delete src/TelegramBot/data/Config.java

--- src/TelegramBot/data/Config.java
+++
@@ -1,9 +0,0 @@
-package TelegramBot.data;
-
-public class Config {
-    protected String dbHost = "localhost";
-    protected String dbPort = "5432";
-    protected String dbPassword = "tgbotking123";
-    protected String dbUser = "postgres";
-    protected String dbName = "users_stat";
-}
